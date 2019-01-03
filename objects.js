c405fe6011e59d0043863b1c49e412315bf8bbf2
algebraic-torrent-6062 <ashley.e.westcott@gmail.com>
2019-01-03 21:46:46 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -4,19 +4,3 @@ function updatePlaylist(playlist, artistName, songTitles) {
   playlist[artistName]=songTitles
 }
 
-function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
-  object[key] = value;
-  return object;
-}
-
-function deleteFromObjectByKey(object, key) {
-  var newObj = Object.assign({}, object);
-  delete newObj[key];
-  return newObj;
-}
-
-
-function destructivelyDeleteFromObjectByKey(object, key) {
-  delete object[key];
-  return object;
-}
